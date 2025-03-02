50f38cbd6de5ac4464061e5cc9e1f64900d57601
Alifuzzaman <nalifuzzaman@gmail.com>
2025-03-02 08:49:46 -0800

    first commit

---
+++ login.js
@@ -0,0 +1,139 @@
+
+document.getElementById("two").addEventListener("click",function(){
+    console.log("hello2")
+    alert("go inside")
+    const amount = document.getElementById("amount").innerText;
+    const convertedamount =parseFloat(amount);
+    const sum=convertedamount-1;
+    console.log(sum)
+    document.getElementById("amount").innerText=sum;
+    const balance=document.getElementById("balance").innerText;
+    const convertedbalance=parseFloat(balance);
+    const sum2=convertedbalance+1;
+    console.log(sum2)
+    document.getElementById("balance").innerText=sum2;
+})
+document.getElementById("three").addEventListener("click",function(){
+    console.log("hello3")
+    alert("go inside")
+    const amount = document.getElementById("amount").innerText;
+    const convertedamount =parseFloat(amount);
+    const sum=convertedamount-1;
+    console.log(sum)
+    document.getElementById("amount").innerText=sum;
+    const balance=document.getElementById("balance").innerText;
+    const convertedbalance=parseFloat(balance);
+    const sum2=convertedbalance+1;
+    console.log(sum2)
+    document.getElementById("balance").innerText=sum2;
+})
+document.getElementById("four").addEventListener("click",function(){
+    console.log("hello4")
+    alert("go inside")
+    const amount = document.getElementById("amount").innerText;
+    const convertedamount =parseFloat(amount);
+    const sum=convertedamount-1;
+    console.log(sum)
+    document.getElementById("amount").innerText=sum;
+    const balance=document.getElementById("balance").innerText;
+    const convertedbalance=parseFloat(balance);
+    const sum2=convertedbalance+1;
+    console.log(sum2)
+    document.getElementById("balance").innerText=sum2;
+})
+document.getElementById("five").addEventListener("click",function(){
+    console.log("hello5")
+    alert("go inside")
+    const amount = document.getElementById("amount").innerText;
+    const convertedamount =parseFloat(amount);
+    const sum=convertedamount-1;
+    console.log(sum)
+    document.getElementById("amount").innerText=sum;
+    const balance=document.getElementById("balance").innerText;
+    const convertedbalance=parseFloat(balance);
+    const sum2=convertedbalance+1;
+    console.log(sum2)
+    document.getElementById("balance").innerText=sum2;
+
+})
+document.getElementById("six").addEventListener("click",function(){
+    console.log("hello6")
+    alert("go inside")
+    const amount = document.getElementById("amount").innerText;
+    const convertedamount =parseFloat(amount);
+    const sum=convertedamount-1;
+    console.log(sum)
+    document.getElementById("amount").innerText=sum;
+    const balance=document.getElementById("balance").innerText;
+    const convertedbalance=parseFloat(balance);
+    const sum2=convertedbalance+1;
+    console.log(sum2)
+    document.getElementById("balance").innerText=sum2;
+})
+document.getElementById("seven").addEventListener("click",function(){
+    console.log("hello7")
+    alert("go inside")
+    const amount = document.getElementById("amount").innerText;
+    const convertedamount =parseFloat(amount);
+    const sum=convertedamount-1;
+    console.log(sum)
+    document.getElementById("amount").innerText=sum;
+    const balance=document.getElementById("balance").innerText;
+    const convertedbalance=parseFloat(balance);
+    const sum2=convertedbalance+1;
+    console.log(sum2)
+    document.getElementById("balance").innerText=sum2;
+})
+document.getElementById("inside-h").addEventListener("click",function(){
+    window.location.href="question.html"
+    alert("go inside")
+})
+document.getElementById("history").addEventListener("click", 
+    function(){
+document.getElementById("his1").style.display="none"
+})
+document.getElementById("history").addEventListener("click", 
+    function(){
+document.getElementById("his2").style.display="none"
+})
+document.getElementById("history").addEventListener("click", 
+    function(){
+document.getElementById("his3").style.display="none"
+})
+document.getElementById("history").addEventListener("click", 
+    function(){
+document.getElementById("his4").style.display="none"
+})
+document.getElementById("history").addEventListener("click", 
+    function(){
+document.getElementById("his5").style.display="none"
+})
+//hjggjgjhgjggjjjh
+document.getElementById("history").addEventListener("click", 
+    function(){
+document.getElementById("his6").style.display="none"
+})
+document.getElementById("two").addEventListener("click", 
+    function(){
+document.getElementById("his1").style.display="block"
+})
+document.getElementById("three").addEventListener("click", 
+    function(){
+document.getElementById("his2").style.display="block"
+})
+document.getElementById("four").addEventListener("click", 
+    function(){
+document.getElementById("his3").style.display="block"
+})
+document.getElementById("five").addEventListener("click", 
+    function(){
+document.getElementById("his4").style.display="block"
+})
+document.getElementById("six").addEventListener("click", 
+    function(){
+document.getElementById("his5").style.display="block"
+})
+document.getElementById("seven").addEventListener("click", 
+    function(){
+document.getElementById("his6").style.display="block"
+})
